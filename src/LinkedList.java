913f1eb9a494daae23370cdb62eb682029cd9240
Omer Cagri Sayir <omercagris@gmail.com>
2024-02-20 21:39:03 +0300

    linked list reverse

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -111,4 +111,19 @@ public class LinkedList {
         }
         return array;
     }
+
+    public void reverse() {
+        var previous = first;
+        var current = first.next;
+        while (current != null) {
+            var next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        last = first;
+        last.next = null;
+        first = previous;
+    }
 }
