7b567d264287cd2ef754fe744d9b70ae14a130f5
Omer Cagri Sayir <omercagris@gmail.com>
2024-02-23 13:01:37 +0300

    Binary Search Tree Validation

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Tree tree = new Tree();
-//        tree.insert(7);
-//        tree.insert(4);
-//        tree.insert(123);
-//        tree.insert(1);
-//        tree.insert(6);
-//        tree.insert(8);
-//        tree.insert(10);
+        tree.insert(7);
+        tree.insert(4);
+        tree.insert(9);
+        tree.insert(1);
+        tree.insert(6);
+        tree.insert(8);
+        tree.insert(10);
 
-        Tree tree2 = new Tree();
-//        tree2.insert(7);
-//        tree2.insert(4);
-//        tree2.insert(123);
-//        tree2.insert(1);
-//        tree2.insert(6);
-//        tree2.insert(8);
-//        tree2.insert(1);
 
-        System.out.println(tree.equals(tree2));
+        System.out.println(tree.isBinarySearchTree());
     }
 }
