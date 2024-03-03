f61c20d9adf8ff3a28ee9d06e7f5f2063c1d4c54
Omer Cagri Sayir <omercagris@gmail.com>
2024-03-03 09:32:46 +0300

    Add BucketSort class.

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] numbers = {7, 3, 1, 4, 6, 2, 3};
-        var sort = new CountingSort();
-        sort.sort(numbers, 7);
+        var sort = new BucketSort();
+        sort.sort(numbers, 3);
         System.out.println(Arrays.toString(numbers));
     }
 }
