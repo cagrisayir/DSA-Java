2ec0579d8652d34c38b0d21835969b460b602373
Omer Cagri Sayir <omercagris@gmail.com>
2024-03-05 06:56:03 +0300

    remove duplicates in string

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(StringUtils.areRotation("ABCD", "CABC"));
+        System.out.println(StringUtils.removeDuplicates("Hellooo!!!"));
     }
 }
--- src/StringUtils.java
+++ src/StringUtils.java
@@ -46,4 +46,20 @@ public class StringUtils {
 
         return true;
     }
+
+    public static String removeDuplicates(String str) {
+        if (str == null) return "";
+        StringBuilder output = new StringBuilder();
+        Set<Character> seen = new HashSet<>();
+
+        for (var ch : str.toCharArray()) {
+            if (!seen.contains(ch)) {
+                seen.add(ch);
+                output.append(ch);
+            }
+        }
+        return output.toString();
+    }
+
+
 }
